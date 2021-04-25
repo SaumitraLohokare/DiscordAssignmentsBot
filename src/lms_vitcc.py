47ddd529e4430aa9fdc23b9c563b1517f4d0c396
Saumitra Lohokare <saumitra.lohokare@gmail.com>
2021-04-26 01:16:15 +0530

    Implemented discord bot

--- src/lms_vitcc.py
+++ src/lms_vitcc.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 url = 'https://lms.vit.ac.in/login/index.php'
 
-def getAssignments(username, password) -> [str]:
+async def getAssignments(username, password) -> str:
     br = mechanize.Browser()
     br.set_handle_equiv(True)
     br.set_handle_redirect(True)
@@ -25,11 +25,28 @@ def getAssignments(username, password) -> [str]:
     mainpage = x.read()
     soup = BeautifulSoup(mainpage, 'lxml')
 
+    max_title = 0
+    max_day = 0
+    max_time = 0
+
     elements = [item for item in soup.find_all(attrs={'data-region': True}) if item['data-region'] == 'event-item']
     assignments = []
     for item in elements:
         title = item.a.text
+        if len(title) > max_title:
+            max_title = len(title)
         day = item.div.a.text
+        if len(day) > max_day:
+            max_day = len(day)
         time = item.div.text.split(', ')[-1]
-        assignments.append(" ".join([title, day, time]))
-    return assignments
+        if len(time) > max_time:
+            max_time = len(time)
+        assignments.append([title, day, time])
+
+    msg = ''
+    for assignment in assignments:
+        msg = msg + f'\n{assignment[0][:30]:30s} | {assignment[1]:20s} | {assignment[2]:10s}'
+    if msg.strip() == '':
+        return 'Incorrect Username or Password.'
+    msg = '```\n' + msg + '\n```'
+    return msg
--- src/main.py
+++ src/main.py
@@ -1,1 +1,83 @@
 import lms_vitcc
+import os
+import json
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+client = discord.Client()
+
+JSON_FILE = 'user_data.json'
+USER_DETAILS = {}
+
+async def get_user_details(message):
+    global USER_DETAILS
+    await message.author.create_dm()
+    if USER_DETAILS.get(str(message.author)) == None:
+        args = message.content.split(' ')
+        try:
+            username, password = args[1:]
+        except Exception:
+            await message.author.dm_channel.send('Incorrect format.')
+            return
+        USER_DETAILS[str(message.author)] = { username: password }
+        print('Added new user.')
+        with open(JSON_FILE) as f:
+            feeds = json.load(f)
+        feeds[str(message.author)] = { username: password }
+        with open(JSON_FILE, mode='w') as f:
+            f.write(json.dumps(feeds))
+        await message.author.dm_channel.send('Detials stored!')
+    else:
+        await message.author.dm_channel.send('Your details are already recorded!')
+
+def load_users():
+    global USER_DETAILS
+    with open(JSON_FILE) as f:
+        USER_DETAILS = json.load(f)
+        print('loaded users:')
+        print(USER_DETAILS)
+
+async def setup_user(user):
+    await user.create_dm()
+    await user.dm_channel.send('Enter your lms username and password in the following format:\n`.details username password`')
+
+async def check_assignments(user):
+    global USER_DETAILS
+    await user.create_dm()
+    await user.dm_channel.send('Retrieving your assignments...')
+    if USER_DETAILS.get(str(user)) != None:
+        username, password = list(USER_DETAILS.get(str(user)).items())[0]
+    else:
+        await user.dm_channel.send('Your details are not recorded... \nUse `.setup`')
+        return
+    assignments = await lms_vitcc.getAssignments(username, password)
+    await user.dm_channel.send(assignments)
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('.'):
+        args = message.content.split(' ')
+        if args[0] == '.setup':
+            await setup_user(message.author)
+        elif args[0] == '.check':
+            await check_assignments(message.author)
+        elif args[0] == '.details':
+            await get_user_details(message)
+
+def main():
+    load_users()
+    client.run(TOKEN)
+
+if __name__ == '__main__':
+    main()
